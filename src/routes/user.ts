d4d6a58ad7b797c0a5a74d155c35a1ac35c66b54
Vlad Tsibizov <vladtsibizov@gmail.com>
2021-02-11 12:22:58 +0300

    fix: поправил ormconfig.json

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import { getConnection } from 'typeorm';
+import User from '../models/User';
 
 const userRoutes = Router();
 
-userRoutes.get('/', (req, res) => {
-  res.json({ foo: 'User route' });
+userRoutes.get('/', async (req, res) => {
+  const result = await getConnection().getRepository(User).find();
+  res.json(result);
 });
 
 export default userRoutes;
