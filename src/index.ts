f72931e60c4775f55a6270d14937b77ab9ecd3c0
Vlad Tsibizov <vladtsibizov@gmail.com>
2021-02-11 11:25:58 +0300

    fix: настроил

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,16 @@
 import express from 'express';
-import 'reflect-metadata';
 import { createConnection } from 'typeorm';
-// eslint-disable-next-line import/no-unresolved
+import 'reflect-metadata';
 import User from './entity/User';
 
 createConnection().then(async (connection) => {
-  console.log('Inserting a new user into the database...');
-  const user = new User();
-  user.firstName = 'Timber';
-  user.lastName = 'Saw';
-  user.age = 25;
-  await connection.manager.save(user);
-  console.log(`Saved a new user with id: ${user.id}`);
+  const userRepository = connection.getRepository(User);
+  const app = express();
 
-  console.log('Loading users from the database...');
-  const users = await connection.manager.find(User);
-  console.log('Loaded users: ', users);
+  app.get('/test', async (req, res) => {
+    const users = await userRepository.find();
+    res.json(users);
+  });
 
-  console.log('Here you can setup and run express/koa/any other framework.');
+  app.listen(8000, () => console.log('listening...'));
 }).catch((error) => console.log(error));
-
-const app = express();
-
-app.get('/', (req, res) => {
-  res.json(123);
-});
-
-app.listen(8000, () => console.log(123));
