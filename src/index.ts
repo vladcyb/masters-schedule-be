c02f7d9ce26e6d165e62daea9c93e8dfe6795472
Vlad Tsibizov <vladtsibizov@gmail.com>
2021-02-18 16:09:09 +0300

    Revert "feat: Настроил express-session"
    This reverts commit 9c1bb080

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,18 @@
 import 'reflect-metadata';
 import express from 'express';
-import session from 'express-session';
 import cors from 'cors';
 import { createConnection } from 'typeorm';
 import routes from './routes';
 import { initializeLocationTypes } from './models/LocationType/initialize';
 
-createConnection()
-  .then(async () => {
-    await initializeLocationTypes();
-    const app = express();
+createConnection().then(async () => {
+  await initializeLocationTypes();
+  const app = express();
 
-    // cross-origin
-    app.use(cors());
+  app.use(cors());
 
-    // session middleware settings
-    const sess = {
-      secret: 'keyboard cat',
-      cookie: { secure: false },
-      resave: false,
-      saveUninitialized: true,
-    };
-    if (app.get('env') === 'production') {
-      app.set('trust proxy', 1); // trust first proxy
-      sess.cookie.secure = true; // serve secure cookies
-    }
-    app.use(session(sess));
+  app.use(express.json());
+  app.use(routes);
 
-    app.use(express.json());
-    app.use(routes);
-
-    app.listen(8000, () => console.log('listening...'));
-  })
-  .catch((error) => console.log(error));
+  app.listen(8000, () => console.log('listening...'));
+}).catch((error) => console.log(error));
