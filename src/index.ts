9c1bb080d49cb27488aa2cb5cfe440a9a2f24f28
Vlad Tsibizov <vladtsibizov@gmail.com>
2021-02-18 15:10:25 +0300

    feat: Настроил express-session

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,35 @@
 import 'reflect-metadata';
 import express from 'express';
+import session from 'express-session';
 import cors from 'cors';
 import { createConnection } from 'typeorm';
 import routes from './routes';
 import { initializeLocationTypes } from './models/LocationType/initialize';
 
-createConnection().then(async () => {
-  await initializeLocationTypes();
-  const app = express();
+createConnection()
+  .then(async () => {
+    await initializeLocationTypes();
+    const app = express();
 
-  app.use(cors());
+    // cross-origin
+    app.use(cors());
 
-  app.use(express.json());
-  app.use(routes);
+    // session middleware settings
+    const sess = {
+      secret: 'keyboard cat',
+      cookie: { secure: false },
+      resave: false,
+      saveUninitialized: true,
+    };
+    if (app.get('env') === 'production') {
+      app.set('trust proxy', 1); // trust first proxy
+      sess.cookie.secure = true; // serve secure cookies
+    }
+    app.use(session(sess));
 
-  app.listen(8000, () => console.log('listening...'));
-}).catch((error) => console.log(error));
+    app.use(express.json());
+    app.use(routes);
+
+    app.listen(8000, () => console.log('listening...'));
+  })
+  .catch((error) => console.log(error));
