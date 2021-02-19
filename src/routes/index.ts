e6e3bdfe728a54eacc0ecb5179aebec7401b350e
Vlad Tsibizov <vladtsibizov@gmail.com>
2021-02-19 14:56:36 +0300

    fix: routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,21 +6,20 @@ import locationRoutes from './location';
 import specializationRoutes from './specialization';
 import serviceRoutes from './service';
 import masterRoutes from './master';
-import clientRoutes from './client';
 import authMiddleware from '../controllers/authMiddleware';
 
 const routes = Router();
-
-/* public routes */
-routes.use('/register', registrationRoutes);
-routes.use('/login', loginRoutes);
-routes.use('/location', locationRoutes);
 
 /* authorized routes */
 routes.use('/order', authMiddleware, orderRoutes);
 routes.use('/specialization', authMiddleware, specializationRoutes);
 routes.use('/service', authMiddleware, serviceRoutes);
 routes.use('/master', authMiddleware, masterRoutes);
-routes.use('/client', authMiddleware, clientRoutes);
+routes.post('/location/*', authMiddleware);
+
+/* public routes */
+routes.use('/register', registrationRoutes);
+routes.use('/login', loginRoutes);
+routes.use('/location', locationRoutes);
 
 export default routes;
--- src/routes/location/index.ts
+++ src/routes/location/index.ts
@@ -1,15 +1,9 @@
 import { Router } from 'express';
 import locationController from '../../controllers/location';
-import authMiddleware from '../../controllers/authMiddleware';
 
 const locationRoutes = Router();
-const privateLocationRoutes = Router();
 
-/* authorized routes */
-privateLocationRoutes.post('/create', locationController.create);
-locationRoutes.use('/create', authMiddleware, privateLocationRoutes);
-
-/* public routes */
+locationRoutes.post('/create', locationController.create);
 locationRoutes.get('/', locationController.get);
 
 export default locationRoutes;
