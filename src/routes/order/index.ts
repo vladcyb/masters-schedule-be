bccdfb9f929b78f6974bef767ef85c49e2a3fa73
Vlad Tsibizov <vladtsibizov@gmail.com>
2021-03-24 12:28:05 +0300

    fix: Запрос на обновление статуса заказа

--- src/routes/order/index.ts
+++ src/routes/order/index.ts
@@ -8,7 +8,7 @@ const orderRoutes = Router();
 const photoUploadMiddleware = multer(multerConfig).single('photo');
 
 orderRoutes.post('/', photoUploadMiddleware, orderController.createOrder);
-orderRoutes.put('/:id/updateStatus', orderController.setOrderStatus);
+orderRoutes.put('/:id/setStatus', orderController.setOrderStatus);
 orderRoutes.put('/:id/setStartDate', orderController.setStartDate);
 orderRoutes.put('/:id/setServices', orderController.setServices);
 orderRoutes.put('/:id/setMaster/:masterId', orderController.setMaster);
